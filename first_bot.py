adf1c42b1f89c1c9614e9cd9528de038676589eb
kfallonwork <katie.fallon@waterstons.com>
2023-10-04 11:10:54 +0100

    print messages

--- first_bot.py
+++ first_bot.py
@@ -37,13 +37,17 @@ if "messages" not in st.session_state:
         {"role": "user", "content": "What is the capital of England?"},
         {"role": "assistant", "content": "I only answer questions about robot dogs"},
     ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+    
 st.chat_message("assistant", avatar=assistant_img).write("Can I interest you in a robot dog?")
 if prompt := st.chat_input():
 
     openai.api_key = st.secrets.api_credentials.api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user", avatar=user_img).write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant", avatar=assistant_img).write(msg.content)
